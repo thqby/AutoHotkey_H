thqby/AutoHotkey_H

commit d3eecea322da9d14911e41cebf934c84585ea295

Fixed HotIf(obj) not finding existing criterion (broken by 581114c1).

source/hotkey.cpp
@@ -190,7 +190,7 @@ HotkeyCriterion *AddHotkeyIfExpr()
 	else
 		g_FirstHotExpr = cp;
 	g_LastHotExpr = cp;
-	return cp;
+	return AddHotkeyCriterion(cp);
 }
 
 

commit 83284e8545a8925b3f3dad14bff1c5077a2fc568

Fix loops in HotkeyCriterion list (broken by d3eecea3)

source/hotkey.cpp
@@ -237,14 +237,6 @@ void Script::PreparseHotkeyIfExpr(Line* aLine)
 		hc->Type = invert ? HOT_IF_NOT_EXIST : HOT_IF_EXIST;
 	hc->WinTitle = param_count > 0 ? postfix[0].marker : _T("");
 	hc->WinText = param_count > 1 ? postfix[1].marker : _T("");
-	// The following adds a duplicate in the event that there are two different expressions
-	// which resolve to the same criterion, such as WinExist("x","") and WinExist("x", "").
-	// In that case, only the first criterion can be referenced by its WinTitle & WinText
-	// (but each can be referenced by its unique expression text).  This seems unavoidable
-	// since variants are only unique to a given expression, and trying to work around that
-	// here would cause inconsistency since this only applies to very specific expressions.
-	// At this stage, the only criterion in the list are those added by the following line:
-	AddHotkeyCriterion(hc);
 }
 
 