da07d3add523b2e7ea7c415122be8e7d12672264
thqby <294633707@qq.com>
2025-02-13 13:46:57 +0800

    Re-implementation "Fixed execution of if-else-if-else-if containing fat arrow functions." (commit 8243f094320526a19de0af5fb9098576b472aa83)

--- source/script.cpp
+++ source/script.cpp
@@ -8431,15 +8431,7 @@ Line *Script::PreparseCommands(Line *aStartingLine)
 					// more fat arrow functions.  It won't work that way because If/Else/Loop/etc.
 					// all skip an initial ACT_BLOCK_BEGIN (to avoid an extra ExecUntil call),
 					// which would result in executing the function's body instead of skipping it.
-					Line *body = line->mNextLine;
-					// Remove the fat arrow functions to allow the correct body to execute.
-					parent->mNextLine = body, body->mPrevLine = parent;
-					// If this wasn't unset, an error dialog would walk upward to find a previous line,
-					// then step forward and fail to find the original target line.  Instead, it will
-					// display from the function's block-begin downward, usually including the expression
-					// which contains the function.  Must not change line->mNextLine or line itself because
-					// they are still needed by the current and next iteration of this loop.
-					block_begin->mPrevLine = nullptr;
+					// 
 					// An alternative approach used in v2.0.17 & .18 was to move the function's body,
 					// but identifying the right place to move it was deceptively complicated:
 					//   if cond
@@ -8456,8 +8448,29 @@ Line *Script::PreparseCommands(Line *aStartingLine)
 					// This was intended to group the lines together so that the debugger can iterate
 					// over them efficiently, but as demonstrated above, they can't always be kept in a
 					// continuguous sequence.  Instead, the debugger now iterates over the function list.
-					// If ever we do shuffle lines around again, be sure that the loop here is redesigned
-					// to finish preparsing this current "line" and continue iterating correctly.
+					// 
+					// ahk_h: To avoid memory leaks, keep the function's body, convert to the following form.
+					//   if cond {
+					//       ; function's body
+					//       f(A()=>)
+					//   } else ...
+					Line *after_body = parent->mRelatedLine;
+					Line *body_end = after_body->mPrevLine;
+					Line *body_start = parent->mNextLine;
+					Line *block_b = new Line(parent->mFileIndex, parent->mLineNumber, ACT_BLOCK_BEGIN, nullptr, 0);
+					Line *block_e = new Line(after_body->mFileIndex, after_body->mLineNumber, ACT_BLOCK_END, nullptr, 0);
+					body_end->mNextLine = after_body->mPrevLine = block_e;
+					block_e->mPrevLine = body_end, block_e->mNextLine = after_body;
+					parent->mNextLine = body_start->mPrevLine = block_b;
+					block_b->mPrevLine = parent, block_b->mNextLine = body_start;
+
+					for (;;)
+					{
+						block_begin->mParentLine = block_b;
+						if (block_begin->mPrevLine->mActionType != ACT_BLOCK_END)
+							break;
+						block_begin = block_begin->mPrevLine->mParentLine;
+					}
 				}
 				else if (parent && parent->mActionType != ACT_BLOCK_BEGIN)
 				{
